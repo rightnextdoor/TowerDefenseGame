8747d6503dff2957f83281313d8da559dede6b95
Kevin Pratt <redhotkbp@hotmail.com>
2021-04-08 12:13:22 -0500

    inital commit

---
+++ Assets/Scripts/LivesUI.cs
@@ -0,0 +1,14 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LivesUI : MonoBehaviour
+{
+    public Text livesText;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        livesText.text = PlayerStats.Lives + " LIVES";
+    }
+}
